fbd25c07729989bcfbf22314301d02713b5ea6f7
Josh Cook <jcook2303@gmail.com>
2023-02-15 07:12:55 -0800

    Some functions practice in js

--- javascript_basics/fundamentals/practice.js
+++ javascript_basics/fundamentals/practice.js
@@ -29,3 +29,55 @@ true + false // 1
 null + 1 // 1
 undefined + 1 // error -> NaN
 " \t \n" - 2 // idk -> -2
+
+// Tasks for Functions: https://javascript.info/function-basics
+
+// Do these two functions perform the same purpose?
+function checkAge(age) {
+    if (age > 18) {
+      return true;
+    } else {
+      // ...
+      return confirm('Did parents allow you?');
+    }
+  }
+
+  function checkAge(age) {
+    if (age > 18) {
+      return true;
+    }
+    // ...
+    return confirm('Did parents allow you?');
+  }
+
+  // Yes because the `else` conditional runs only if `age` is less than 18, returning the second function in both instances
+
+
+  // Rewrite `checkAge` in a single line without the `if` conditional twice, using ? and ||
+  function checkAge(age) {
+    if (age > 18) {
+      return true;
+    } else {
+      return confirm('Did parents allow you?');
+    }
+  }
+  // ?
+  function checkAge(age) {
+    return age > 18 ? true : confirm('Did parents allow you?');
+  }
+  // ||
+  function checkAge(age) {
+    return age > 18 || confirm('Did parents alow you?');
+  }
+
+  // Write a function called min(a,b) which returns the lesser value of the given parameters
+  function min(a, b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+  } // Or can be rewritten as:
+  function min(a, b) {
+    return a < b ? a : b;
+  }
